5baa14c40673c8d644ba0a061eb3cb2ffaf390fa
Miraziz <mirazizbek701@gmail.com>
2023-11-05 22:03:16 +0500

    update

--- TgBotProject.Application/Services/UniversityService.cs
+++ TgBotProject.Application/Services/UniversityService.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using TgBotProject.Application.Interfaces;
@@ -10,9 +12,21 @@ namespace TgBotProject.Application.Services;
 
 public class UniversityService : IUniversityService
 {
-    public Task<University> GetUniversities()
+    private readonly HttpClient _httpClient;
+    private readonly string _url;
+    public UniversityService(HttpClient httpClient, IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        _httpClient = httpClient;
+        _url = configuration.GetSection("BaseAddress")["universityApi"];
+        _httpClient.BaseAddress = new Uri($"{_url}");
+    }
+
+    public async Task<University> GetUniversities()
+    {
+        var response = await _httpClient.GetAsync("");
+        var responseModel = await response.Content.ReadFromJsonAsync<University>();
+
+        return responseModel;
     }
 
     public Task<string> GetUniversity()
